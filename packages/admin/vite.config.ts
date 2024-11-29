c2b2c68184856957f031e129066d016bd4846514
Florian Ferbach <florian@bleumatin.fr>
2024-11-29 15:03:56 +0100

    Fix admin deployment

--- packages/admin/vite.config.ts
+++ packages/admin/vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgr from "vite-plugin-svgr";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: process.env.PUBLIC_URL,
   plugins: [svgr(), react()],
   server: {
     port: 4002,
   },
-})
+});
